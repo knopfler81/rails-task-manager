b2fee778ef62103cee5bee7af86bcee142943cad
Nelly Duclos <knopfler42@yahoo.fr>
2017-02-14 11:48:43 +0100

    Completed task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+
+  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+  def set_tasks
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+  # get '/tasks'            => 'tasks#index'
+  # get '/tasks/new'        => 'tasks#new'
+  # get '/task/:id'        => 'tasks#show'
+  # post '/tasks'           => 'tasks#index'
+  # get '/tasks/:id/edit'   => 'tasks#edit'
+  # patch '/tasks/:id'      => 'tasks#update'
+  # delete '/tasks/:id'     => 'tasks#destroy'
 end
